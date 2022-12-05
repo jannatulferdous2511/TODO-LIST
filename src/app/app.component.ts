16c5aad1b76ac7abf4eeeb8248b95e07c8fa0eee
JannatulFerdous <jannatulferdous2511@gmail.com>
2022-12-05 20:13:10 +0600

    first update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Todo-List';
+  tasks = [
+    'Visit Ann',
+    'Call Dad',
+    'go to gym',
+    'wash the dishes',
+    'shop for the party',
+  ];
+  add(newTask: string) {
+    this.tasks.push(newTask);
+  }
+  remove(existingTask: string) {
+    var userconfirmed = confirm(
+      `are you sure you want  to remove the following task? \n "${existingTask}"`
+    );
+    this.tasks = this.tasks.filter((task) => task != existingTask);
+  }
+  markAsDone(task: string) {
+    alert(`The task:"` + task + `" is done`);
+  }
 }
